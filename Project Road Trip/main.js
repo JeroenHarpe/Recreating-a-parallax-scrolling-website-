d1ce4a2990b01743b8f5c220781122cbe15385ef
Jeroen Harpe <43515079+JeroenHarpe@users.noreply.github.com>
2019-03-20 16:34:44 +0800

    First week finished

---
+++ Project Road Trip/main.js
@@ -0,0 +1,29 @@
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+function onLoad() {
+
+  var today = new Date();
+  var dd = today.getDate();
+  var mm = today.getMonth()+1;
+  var yyyy = today.getFullYear();
+
+  if(dd<10) {
+  dd = '0'+dd
+  } 
+
+  if(mm<10) {
+  mm = '0'+mm
+  } 
+
+  today = dd + '/' + mm + '/' + yyyy;
+  document.getElementById("date").innerHTML = today;
+
+}
